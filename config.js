bc4b60f6eab3340ff31ace91ed8b4972c7f1ff04
Zeroxx2007 <kervin20xd@gmail.com>
2025-03-30 10:55:21 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+module.exports = {
+  ownerNumber: '51999999999@c.us', // Tu número de WhatsApp (ej: 51999999999@c.us)
+  prefix: '.', // Prefijo para los comandos (ej: !hola, !menu)
+};
